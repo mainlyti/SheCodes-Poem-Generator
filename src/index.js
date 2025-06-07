9b6cc202d6fd84e28a3e35cd91d1ee27d6d8ad3f
mainlyti <mainlyti@gmail.com>
2025-06-07 19:50:00 -0400

    edited context

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let key = "5bfa080034fb64td6b864b3a813efo04";
   let prompt = `Generate a short poem about ${userInput.value}`;
   let context =
-    "Create a short, four line poem that is lighthearted and sweet based on the user input. Seperate each line of the poem using <br>. Sign the poem at the bottom with a single emoji that relates to the poem.";
+    "Create a short poem, between 2 to 4 lines long, that is lighthearted and sweet based on the user input. Seperate each line of the poem using <br>. Sign the poem at the bottom with a single emoji that relates to the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
   axios.get(apiUrl).then(displayPoem);
