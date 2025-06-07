5fc070bd5bfdd8ff122f0a3973d05dd0158778d9
mainlyti <mainlyti@gmail.com>
2025-06-07 19:47:17 -0400

    added axios, the AI api, and js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "Hello",
-    autoStart: true,
-    cursor: "",
-  });
+  let userInput = document.querySelector("#user-prompt");
+
+  let key = "5bfa080034fb64td6b864b3a813efo04";
+  let prompt = `Generate a short poem about ${userInput.value}`;
+  let context =
+    "Create a short, four line poem that is lighthearted and sweet based on the user input. Seperate each line of the poem using <br>. Sign the poem at the bottom with a single emoji that relates to the poem.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let peomFormElement = document.querySelector("#poem-form");
